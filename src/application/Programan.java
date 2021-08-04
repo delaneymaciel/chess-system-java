c82a811b25bde56c942aeb1b799feaab64f96be7
Delaney Maciel <delaneymaciel@hotmail.com>
2021-08-03 23:30:41 -0300

    Starting to implement Board and Piece

--- src/application/Programan.java
+++ src/application/Programan.java
@@ -1,5 +1,6 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Programan {
@@ -7,6 +8,8 @@ public class Programan {
 	public static void main(String[] args) {
 		Position pos = new Position(3, 5);
 		System.out.println(pos);
+		
+		Board bord = new Board(8, 8);
 
 	}
 
