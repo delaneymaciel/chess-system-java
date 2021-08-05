ba1c47ae297025c39589d93dab911340193b16d2
Delaney Maciel <delaneymaciel@hotmail.com>
2021-08-04 22:59:13 -0300

    Chess layer and printing the board

--- src/application/Programan.java
+++ src/application/Programan.java
@@ -2,14 +2,18 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Programan {
 
 	public static void main(String[] args) {
 		Position pos = new Position(3, 5);
-		System.out.println(pos);
 		
 		Board bord = new Board(8, 8);
+		
+		ChessMatch chessMath = new ChessMatch();
+		
+		UI.printBoard(chessMath.getPieces());
 
 	}
 
