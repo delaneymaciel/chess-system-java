df0d49f507c7051a551b94965373a4b77c4874aa
Delaney Maciel <delaneymaciel@hotmail.com>
2021-08-09 19:20:51 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoardExcpetion.java
@@ -0,0 +1,12 @@
+package boardgame;
+
+public class BoardExcpetion extends RuntimeException{
+
+
+	private static final long serialVersionUID = 1L;
+	
+	public BoardExcpetion(String msg) {
+		super(msg);
+	}
+
+}
